126c991e4ac69b803dcda21787b44eae7ac82f42
usman.malik46_ <42khan0@gmail.com>
2023-06-23 18:31:21 +0500

    update

--- src/components/About Data/AboutW.js
+++ src/components/About Data/AboutW.js
@@ -7,7 +7,7 @@ import AboutCard from "./AboutCard";
 const AboutW = () => {
   return (
     <div className="work-containers">
-      <h1 className="project-headings">Projects</h1>
+      <h1 className="project-headings">Education</h1>
       <div className="project-containers">
         {AboutCardData.map((val, index) => {
           return (
